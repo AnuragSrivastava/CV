9c4a7f03df8c0c89d4fddd424b36d30c4ab37ae8
Anurag Srivastava <anurag.srivastava@gmx.com>
2016-04-06 00:43:02 +0200

    core_functionalities

--- CV/core_functions.py
+++ CV/core_functions.py
@@ -4,7 +4,7 @@ import numpy as np
 image = cv2.imread('face.tiff')
 
 # (Rows, Columns, Channels)
-r, c, _ = image.shape
+r, c, ch = image.shape
 
 # getting regions of interest (roi)
 
@@ -15,5 +15,23 @@ image[:, (3*c/4):c] = roi1
 image[:, 0:c/4] = roi2
 
 cv2.imshow("Image", image)
+
+# add images
+
+img = cv2.imread('normal.jpg')
+
+roi = image[:, :]
+bw_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ret, mask = cv2.threshold(bw_image, 10, 255, cv2.THRESH_BINARY)
+mask_inv = cv2.bitwise_not(mask)
+
+foreground = cv2.bitwise_and(roi, roi, mask = mask)
+background = cv2.bitwise_and(roi, roi, mask = mask_inv)
+
+img_sum = cv2.add(foreground, background)
+img[0:r, 0:c] = img_sum
+
+cv2.imshow('Sum', img)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
