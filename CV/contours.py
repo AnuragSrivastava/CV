f64091c35d5e6248d675173b1c726b27e0c52c19
Anurag Srivastava <anurag.srivastava@gmx.com>
2016-04-08 20:00:05 +0200

    contours+moments

--- CV/contours.py
+++ CV/contours.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def nothing(x):
     pass
@@ -34,12 +35,30 @@ contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_N
 cv2.drawContours(image, contours, -1, (0, 0, 255), 3)
 
 print hierarchy
+print contours
 
 # display the contours
 cv2.imshow('found_contours', image)
 
+# getting image moments (its a dictionary)
+cnt = contours[0]
+moment = cv2.moments(cnt)
 
+print moment
+print "first contour is: ", cnt
+print "Area of 1st contour: ", int(moment['m00'])
 
+cx = int(moment['m10']/moment['m00'])
+cy = int(moment['m01']/moment['m00'])
+print "centroid of first contour : (", cx, ",", cy, ")"
+
+# bounding rectangle (x,y) : top left coordinate
+x, y, w, h = cv2.boundingRect(cnt)
+print np.shape(image)
+print x, y, w, h
+
+cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 5)
+cv2.imshow('bounded 1st contour', image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
